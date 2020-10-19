758d6a6fa8c08830b45de48323ac6eb28ccff382
cmanthou <cory.manthou@gmail.com>
2020-10-19 17:25:59 -0400

    fixed server

--- server.js
+++ server.js
@@ -4,3 +4,24 @@ const apiRoutes = require("./routes/index");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+//API Route
+app.use('/api', apiRoutes);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+mongoose.connect(process.envMONGODB_URI || "mongodb://localhost/googlebooks")
+
+app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
