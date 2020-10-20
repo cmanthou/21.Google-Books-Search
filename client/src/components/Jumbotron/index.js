f8e0c1a659f1361638873105f4310c703bdc2797
cmanthou <cory.manthou@gmail.com>
2020-10-19 21:37:31 -0400

    client side

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron text-center">
+            <h1>React Google Book Search</h1>
+            <h3>Search and Save Books!</h3>
+        </div>
+    );
+}
+export default Jumbotron;
